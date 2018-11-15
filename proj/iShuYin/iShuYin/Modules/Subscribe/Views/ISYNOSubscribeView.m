1244af2fd7372a08503e636ac90ee3308d5257b6
李艺真 <115052@nd>
2018-11-15 18:39:52 +0800

    订阅

---
+++ proj/iShuYin/iShuYin/Modules/Subscribe/Views/ISYNOSubscribeView.m
@@ -0,0 +1,21 @@
+//
+//  ISYNOSubscribeView.m
+//  iShuYin
+//
+//  Created by ND on 2018/11/15.
+//  Copyright © 2018年 ishuyin. All rights reserved.
+//
+
+#import "ISYNOSubscribeView.h"
+
+@implementation ISYNOSubscribeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
